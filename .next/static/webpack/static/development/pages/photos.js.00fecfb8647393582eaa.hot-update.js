b7a5f704b5a76aa88fda45d2f5e27a4fe649d9a2
Katherine Connell <katherine.connell@willowtreeapps.com>
2019-06-28 12:51:53 -0400

    package json update

---
+++ .next/static/webpack/static/development/pages/photos.js.00fecfb8647393582eaa.hot-update.js
@@ -0,0 +1,37 @@
+webpackHotUpdate("static/development/pages/photos.js",{
+
+/***/ "./components/Popups/ModalPopup.jsx":
+/*!******************************************!*\
+  !*** ./components/Popups/ModalPopup.jsx ***!
+  \******************************************/
+/*! exports provided: ModalPopup */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ModalPopup", function() { return ModalPopup; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+var ModalPopup = function ModalPopup(body) {
+  var background = Object(react__WEBPACK_IMPORTED_MODULE_0__["useRef"])(body);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
+    action: "mailto:ktconnell99@gmail.com",
+    method: "POST",
+    enctype: "multipart/form-data",
+    name: "EmailTestForm"
+  }, "Your Name:", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    type: "text",
+    name: "VisitorName"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), "Your Comment:", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("textarea", {
+    name: "VisitorComment"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    type: "submit",
+    value: "Email This Form"
+  })));
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=photos.js.00fecfb8647393582eaa.hot-update.js.map
---
+++ env.ts
@@ -0,0 +1,4 @@
+export default {
+    API_SPACE_KEY: '7nio64lc6xum',
+    API_ACCESS_TOKEN: '2BRt1UfcIexpHhwfkP98K0ydPgIB8vATWysOcDrOI78'
+}
